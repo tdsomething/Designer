7e7dd6287859b0972da1f4ce043239522d0f3df1
protagonisths <13837050616@163.com>
2021-12-27 13:12:44 +0800

    feat(style): unify code style

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,11 @@ import unpluginVueComponents from 'unplugin-vue-components/vite'
 import { NaiveUiResolver } from 'unplugin-vue-components/resolvers'
 
 export default defineConfig({
-  plugins: [vue(), unpluginVueComponents({
-    resolvers: [ NaiveUiResolver() ],
-    dts: true
-  })]
+  plugins: [
+    vue(),
+    unpluginVueComponents({
+      resolvers: [NaiveUiResolver()],
+      dts: true
+    })
+  ]
 })
