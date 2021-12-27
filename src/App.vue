1df44adc6237663a834f3bcfe058f86a702d17b1
protagonisths <13837050616@163.com>
2021-12-27 16:57:39 +0800

    feat(style): clean

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,4 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-</script>
+<script setup lang="ts"></script>
 
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
