9ad9eda4bf0a64d1bfc5243c1708a6b9e13940fd
protagonisths <13837050616@163.com>
2022-01-07 20:37:06 +0800

    feat(depend): install tailwindcss

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,6 @@
 <script setup lang="ts"></script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
   <n-button>naive-ui</n-button>
 </template>
 
@@ -13,6 +11,5 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,14 @@ import vue from '@vitejs/plugin-vue'
 import unpluginVueComponents from 'unplugin-vue-components/vite'
 import { NaiveUiResolver } from 'unplugin-vue-components/resolvers'
 import unpluginAutoImport from 'unplugin-auto-import/vite'
+import { resolve } from 'path'
 
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src')
+    }
+  },
   plugins: [
     vue(),
     unpluginAutoImport(),
