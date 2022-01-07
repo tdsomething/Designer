9c1730d5eed22475e7d06d3eb01a424030deef65
protagonisths <13837050616@163.com>
2022-01-07 22:02:49 +0800

    feat(view): init login page

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import unpluginVueComponents from 'unplugin-vue-components/vite'
-import { NaiveUiResolver } from 'unplugin-vue-components/resolvers'
+import { NaiveUiResolver, HeadlessUiResolver } from 'unplugin-vue-components/resolvers'
 import unpluginAutoImport from 'unplugin-auto-import/vite'
 import { resolve } from 'path'
 
@@ -15,7 +15,15 @@ export default defineConfig({
     vue(),
     unpluginAutoImport(),
     unpluginVueComponents({
-      resolvers: [NaiveUiResolver()],
+      resolvers: [
+        NaiveUiResolver(),
+        HeadlessUiResolver({ prefix: '' })
+        // (name) => {
+        //   if (name === 'LockClosedIcon') {
+        //     return { importName: name.slice(0), path: '@heroicons/vue/solid' }
+        //   }
+        // }
+      ],
       dts: true
     })
   ]
