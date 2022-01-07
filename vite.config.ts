9ed616c4cccaae622acdcca5b6499d1e7c4ddab0
protagonisths <13837050616@163.com>
2022-01-07 19:07:48 +0800

    feat(package): install unpluginAutoImport

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,12 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import unpluginVueComponents from 'unplugin-vue-components/vite'
 import { NaiveUiResolver } from 'unplugin-vue-components/resolvers'
+import unpluginAutoImport from 'unplugin-auto-import/vite'
 
 export default defineConfig({
   plugins: [
     vue(),
+    unpluginAutoImport(),
     unpluginVueComponents({
       resolvers: [NaiveUiResolver()],
       dts: true
